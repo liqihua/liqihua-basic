f6996efa3600e0608b600674b51c2f5537200326
liqihua <iamliqihua@qq.com>
2018-10-15 15:56:36 +0800

    liqihua

--- src/main/java/com/liqihua/config/advice/SysControllerAdvice.java
+++ src/main/java/com/liqihua/config/advice/SysControllerAdvice.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
