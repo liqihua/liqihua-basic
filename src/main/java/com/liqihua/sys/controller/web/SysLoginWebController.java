63ffa72b0b5e80e655c195267a870f2230dd3141
liqihua <iamliqihua@qq.com>
2018-11-07 15:41:43 +0800

    liqihua

---
+++ src/main/java/com/liqihua/sys/controller/web/SysLoginWebController.java
@@ -0,0 +1,48 @@
+package com.liqihua.sys.controller.web;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.liqihua.common.basic.BaseController;
+import com.liqihua.common.basic.WebResult;
+import com.liqihua.common.utils.Tool;
+import com.liqihua.sys.entity.SysUserEntity;
+import com.liqihua.sys.entity.vo.SysUserVO;
+import com.liqihua.sys.service.SysUserService;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ *  用户 前端控制器
+ * </p>
+ *
+ * @author liqihua
+ * @since 2018-11-07
+ */
+@RestController
+@RequestMapping("/sys/sysLoginWebController")
+public class SysLoginWebController extends BaseController {
+    @Resource
+    private SysUserService sysUserService;
+
+
+
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public WebResult login(@ApiParam(value = "id",required = true) @RequestParam(value="id",required = true) Long id){
+        boolean delete = sysUserService.removeById(id);
+        return buildSuccessInfo(delete);
+    }
+
+
+
+
+}
