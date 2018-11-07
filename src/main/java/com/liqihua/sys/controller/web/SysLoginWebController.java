a69c45551f201c8b0b6916010a656d3e1c93d062
liqihua <iamliqihua@qq.com>
2018-11-07 15:58:30 +0800

    liqihua

--- src/main/java/com/liqihua/sys/controller/web/SysLoginWebController.java
+++ src/main/java/com/liqihua/sys/controller/web/SysLoginWebController.java
@@ -1,15 +1,19 @@
 package com.liqihua.sys.controller.web;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.liqihua.common.basic.BaseController;
 import com.liqihua.common.basic.WebResult;
 import com.liqihua.common.utils.Tool;
+import com.liqihua.crud.controller.api.TestApiController;
 import com.liqihua.sys.entity.SysUserEntity;
 import com.liqihua.sys.entity.vo.SysUserVO;
 import com.liqihua.sys.service.SysUserService;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +34,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/sys/sysLoginWebController")
 public class SysLoginWebController extends BaseController {
+    private static final Logger LOG = LoggerFactory.getLogger(SysLoginWebController.class);
+
     @Resource
     private SysUserService sysUserService;
 
@@ -37,9 +43,10 @@ public class SysLoginWebController extends BaseController {
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public WebResult login(@ApiParam(value = "id",required = true) @RequestParam(value="id",required = true) Long id){
-        boolean delete = sysUserService.removeById(id);
-        return buildSuccessInfo(delete);
+    public WebResult login(@RequestParam(required = true) String username,
+                           @RequestParam(required = true) String password){
+        LOG.info("username:"+username+",password:"+password);
+        return buildSuccessInfo(RandomUtil.randomString(32));
     }
 
 
