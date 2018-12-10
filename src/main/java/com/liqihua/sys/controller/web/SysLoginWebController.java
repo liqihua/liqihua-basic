59d10b52bf00e397794f0f4e909887dffedd9f48
liqihua <iamliqihua@qq.com>
2018-12-10 20:38:03 +0800

    liqihua

--- src/main/java/com/liqihua/sys/controller/web/SysLoginWebController.java
+++ src/main/java/com/liqihua/sys/controller/web/SysLoginWebController.java
@@ -1,6 +1,13 @@
 package com.liqihua.sys.controller.web;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSON;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.liqihua.common.basic.BaseController;
 import com.liqihua.common.basic.WebResult;
 import com.liqihua.sys.entity.vo.SysUserVO;
@@ -13,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * <p>
@@ -30,7 +38,22 @@ public class SysLoginWebController extends BaseController {
     @Resource
     private SysUserService sysUserService;
 
+    /*public static void main(String[] args) {
+        String userId = "abc";
+        String secret = "123yyuuii";
+        Date expireTime = DateUtil.offsetMinute(new Date(),1);
+        String token = JWT.create().withExpiresAt(expireTime).withClaim("userId",userId).sign(Algorithm.HMAC256(secret));
 
+
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).build();
+        DecodedJWT jwt = verifier.verify(token);
+        System.out.println(JSON.toJSONString(jwt));
+        //jwt = JWT.decode(token);
+        Claim claim = jwt.getClaim("userId");
+        String str = claim.asString();
+        System.out.println(JSON.toJSONString(claim));
+        System.out.println(str);
+    }*/
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
