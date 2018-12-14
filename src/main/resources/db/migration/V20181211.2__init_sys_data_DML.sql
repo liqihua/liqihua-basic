a822a4f55102ba2eb0bac569b2cb6e55ca311d45
liqihua <iamliqihua@qq.com>
2018-12-14 11:32:05 +0800

    liqihua

--- src/main/resources/db/migration/V20181211.2__init_sys_data_DML.sql
+++ src/main/resources/db/migration/V20181211.2__init_sys_data_DML.sql
@@ -14,7 +14,6 @@ INSERT INTO `sys_perm` VALUES ('13', '用户列表', 'sysUser-list', '', '2018-1
 INSERT INTO `sys_perm` VALUES ('14', '保存用户', 'sysUser-save', '', '2018-12-12 16:31:15', '2018-12-12 16:31:15');
 INSERT INTO `sys_perm` VALUES ('15', '删除用户', 'sysUser-delete', '', '2018-12-12 16:31:50', '2018-12-12 16:31:50');
 INSERT INTO `sys_perm` VALUES ('16', '设置角色', 'sysUser-setRole', '', '2018-12-12 16:33:22', '2018-12-12 16:33:22');
-INSERT INTO `sys_perm` VALUES ('17', '菜单列表', 'sysMenu-list', '', '2018-12-12 16:34:21', '2018-12-12 16:34:21');
 INSERT INTO `sys_perm` VALUES ('18', '保存菜单', 'sysMenu-save', '', '2018-12-12 16:35:34', '2018-12-12 16:35:34');
 INSERT INTO `sys_perm` VALUES ('19', '删除菜单', 'sysMenu-delete', '', '2018-12-12 16:35:57', '2018-12-12 16:35:57');
 INSERT INTO `sys_perm` VALUES ('20', '权限列表', 'sysPerm-list', '', '2018-12-12 16:37:25', '2018-12-12 16:37:25');
@@ -42,7 +41,6 @@ INSERT INTO `sys_perm_menu` VALUES ('11', '24', '13', '2018-12-12 16:30:40', '20
 INSERT INTO `sys_perm_menu` VALUES ('12', '24', '14', '2018-12-12 16:31:15', '2018-12-12 16:31:15');
 INSERT INTO `sys_perm_menu` VALUES ('13', '24', '15', '2018-12-12 16:31:50', '2018-12-12 16:31:50');
 INSERT INTO `sys_perm_menu` VALUES ('14', '24', '16', '2018-12-12 16:33:22', '2018-12-12 16:33:22');
-INSERT INTO `sys_perm_menu` VALUES ('15', '25', '17', '2018-12-12 16:34:21', '2018-12-12 16:34:21');
 INSERT INTO `sys_perm_menu` VALUES ('16', '25', '18', '2018-12-12 16:35:34', '2018-12-12 16:35:34');
 INSERT INTO `sys_perm_menu` VALUES ('17', '25', '19', '2018-12-12 16:35:57', '2018-12-12 16:35:57');
 INSERT INTO `sys_perm_menu` VALUES ('18', '26', '20', '2018-12-12 16:37:25', '2018-12-12 16:37:25');
@@ -65,20 +63,19 @@ INSERT INTO `sys_role_menu` VALUES ('5', '1', '27', '2018-12-12 16:42:27', '2018
 -- ----------------------------
 -- Records of sys_role_perm
 -- ----------------------------
-INSERT INTO `sys_role_perm` VALUES ('1', '1', '13', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('2', '1', '14', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('3', '1', '15', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('4', '1', '16', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('5', '1', '17', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('6', '1', '18', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('7', '1', '19', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('8', '1', '22', '2018-12-12 16:42:27', '2018-12-12 16:42:27');
-INSERT INTO `sys_role_perm` VALUES ('9', '1', '21', '2018-12-12 16:42:28', '2018-12-12 16:42:28');
-INSERT INTO `sys_role_perm` VALUES ('10', '1', '20', '2018-12-12 16:42:28', '2018-12-12 16:42:28');
-INSERT INTO `sys_role_perm` VALUES ('11', '1', '23', '2018-12-12 16:42:28', '2018-12-12 16:42:28');
-INSERT INTO `sys_role_perm` VALUES ('12', '1', '24', '2018-12-12 16:42:28', '2018-12-12 16:42:28');
-INSERT INTO `sys_role_perm` VALUES ('13', '1', '25', '2018-12-12 16:42:28', '2018-12-12 16:42:28');
-INSERT INTO `sys_role_perm` VALUES ('14', '1', '26', '2018-12-12 16:42:28', '2018-12-12 16:42:28');
+INSERT INTO `sys_role_perm` VALUES ('121', '1', '14', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('122', '1', '15', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('123', '1', '16', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('124', '1', '18', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('125', '1', '19', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('126', '1', '20', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('127', '1', '21', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('128', '1', '22', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('129', '1', '23', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('130', '1', '24', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('131', '1', '25', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('132', '1', '26', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
+INSERT INTO `sys_role_perm` VALUES ('133', '1', '13', '2018-12-14 11:06:06', '2018-12-14 11:06:06');
 
 -- ----------------------------
 -- Records of sys_role_user
